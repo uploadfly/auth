1d2c90949842c637ecd084c1c4f05fd894476eda
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 14:48:30 +0100

    generate OTP and send email

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import prisma from "../../prisma";
 import validator from "validator";
+import { generateOTP } from "../../utils/generateOtp";
+import { sendEmail } from "../../utils/sendEmail";
+import bcrypt from "bcrypt";
 
 const signup = async (req: Request, res: Response) => {
   const { email, password, confirmPassword } = req.body;
@@ -22,11 +25,20 @@ const signup = async (req: Request, res: Response) => {
     return res.status(400).json({ message: "Passwords do not match" });
   }
 
+  const otp = generateOTP();
+
+  await sendEmail({
+    to: email,
+    subject: "Signup OTP",
+    body: `Your OTP is ${otp}`,
+  });
+
   await prisma.user.create({
     data: {
       email,
       username: email.split("@")[0],
-      password,
+      password: bcrypt.hashSync(password, 10),
+      otp,
     },
   });
 
