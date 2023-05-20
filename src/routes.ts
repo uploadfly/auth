cac0a0e47201ae8a254a06961fe7ee76fd130d3c
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 22:31:45 +0100

    added login route

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import expres from "express";
 import { signup } from "./controllers/signup";
+import { login } from "./controllers/login";
 
 const router = expres.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
