5cbe62f2d9c2d05996cb2e4c6324581d71d52fb8
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 22:42:48 +0100

    /verify

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import expres from "express";
 import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
+import { verifyEmail } from "./controllers/verify";
 
 const router = expres.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.put("/verify", verifyEmail);
 
 export default router;
