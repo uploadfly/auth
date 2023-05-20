a1eac7f25ee228778082aa4d4c29afc689e6d926
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 14:11:01 +0100

    test plunk

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import bodyParser from "body-parser";
 import express from "express";
 import authRouter from "./src/routes";
+import { sendEmail } from "./utils/sendEmail";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  (async () => {
+    await sendEmail({
+      to: "akinkunmioye42@gmail.com",
+      subject: "Hey",
+      body: "Hello there",
+    });
+    res.send("Email sent");
+  })();
+});
 
 app.use("/", authRouter);
 
