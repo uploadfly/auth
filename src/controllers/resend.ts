1df926931551620a17d497fb3e639d1e41598cd6
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-21 16:12:59 +0100

    resend OTP

---
+++ src/controllers/resend.ts
@@ -0,0 +1,48 @@
+import { Request, Response } from "express";
+import prisma from "../../prisma";
+import { generateOTP } from "../../utils/generateOtp";
+import { sendEmail } from "../../utils/sendEmail";
+import dayjs from "dayjs";
+
+const resendOTP = async (req: Request, res: Response) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(400).json({ message: "Email is required" });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "There is no account associated with this email",
+    });
+  }
+
+  if (user.email_verified) {
+    return res.status(400).json({
+      message: "Email is already verified",
+    });
+  }
+
+  const otp = generateOTP();
+
+  await sendEmail({
+    to: email,
+    subject: "Verify your email",
+    body: `Your verification code is ${otp}`,
+  });
+
+  prisma.user.update({
+    where: {
+      email,
+    },
+    data: {
+      otp,
+      otp_expiry: dayjs().add(30, "minutes").toISOString(),
+    },
+  });
+};
+export { resendOTP };
