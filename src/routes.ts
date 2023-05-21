3c132f92fc8a918deb2c0b61fa35f7b733a3b911
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-21 16:59:45 +0100

    added /verify/resend

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import expres from "express";
 import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
 import { verifyEmail } from "./controllers/verify";
+import { resendOTP } from "./controllers/resend";
 
 const router = expres.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.put("/verify", verifyEmail);
+router.put("/verify/resend", resendOTP);
 
 export default router;
