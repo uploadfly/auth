10157b179cc71d73995ea46b29b13873cb5d205c
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-22 05:04:02 +0100

    auth config

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,10 @@ import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
 import { verifyEmail } from "./controllers/verify";
 import { resendOTP } from "./controllers/resend";
+// import { githubAuth } from "./controllers/github";
+// import passport from "passport";
+
+import passportConfig from "./configs/passport";
 
 const router = expres.Router();
 
@@ -10,5 +14,5 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.put("/verify", verifyEmail);
 router.put("/verify/resend", resendOTP);
-
+router.get("/github", passportConfig.authenticate("github"));
 export default router;
