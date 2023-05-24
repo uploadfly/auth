dbc51d7d9a0e8d1af0b1193b2566de1c03432fb4
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-25 00:43:39 +0100

    ...

--- index.ts
+++ index.ts
@@ -2,10 +2,19 @@ import bodyParser from "body-parser";
 import express from "express";
 import authRouter from "./src/routes";
 import passport from "./src/configs/passport";
+import cookieSession from "cookie-session";
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["key1", "key2"],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send(
@@ -16,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", authRouter);
 
