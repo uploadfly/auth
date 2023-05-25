2552d8dc55fe996d6a37ee1e7fb2174accaa38dc
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-25 02:17:02 +0100

    /github/callback

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,8 @@ import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
 import { verifyEmail } from "./controllers/verify";
 import { resendOTP } from "./controllers/resend";
-
-import passportConfig from "./configs/passport";
-import { githubCallback } from "./controllers/github-callback";
+import { githubAuth } from "./controllers/github";
+import { githubAuthCallback } from "./controllers/github/callback";
 
 const router = expres.Router();
 
@@ -13,11 +12,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.put("/verify", verifyEmail);
 router.put("/verify/resend", resendOTP);
-router.get(
-  "/github",
-  passportConfig.authenticate("github", {
-    scope: ["user:email"],
-  })
-);
-router.get("/github/callback", githubCallback);
+router.get("/github", githubAuth);
+router.get("/github/callback/", githubAuthCallback);
+
 export default router;
