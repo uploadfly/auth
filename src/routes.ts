117dd29d6d2176caa49fc39ee0f33ccedd412ce2
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-26 01:50:12 +0100

    logout

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import expres from "express";
+import expres, { Request, Response } from "express";
 import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
 import { verifyEmail } from "./controllers/verify";
@@ -14,5 +14,12 @@ router.put("/verify", verifyEmail);
 router.put("/verify/resend", resendOTP);
 router.get("/github", githubAuth);
 router.get("/github/callback/", githubAuthCallback);
+router.post("/logout", async (req: Request, res: Response) => {
+  res.cookie("access_token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.sendStatus(200);
+});
 
 export default router;
