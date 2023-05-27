93614c55807694d5d606e085e9dc87f5ddf0b53f
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-27 19:58:52 +0100

    protected

--- src/routes.ts
+++ src/routes.ts
@@ -5,9 +5,13 @@ import { verifyEmail } from "./controllers/verify";
 import { resendOTP } from "./controllers/resend";
 import { githubAuth } from "./controllers/github";
 import { githubAuthCallback } from "./controllers/github/callback";
+import { protect } from "./middlewares/authMiddleware";
 
 const router = expres.Router();
 
+router.route("/").get(protect, (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 router.post("/signup", signup);
 router.post("/login", login);
 router.put("/verify", verifyEmail);
@@ -20,6 +24,10 @@ router.post("/logout", async (req: Request, res: Response) => {
     expires: new Date(0),
   });
   res.sendStatus(200);
+});
+
+router.get("/user", protect, (req: Request, res: Response) => {
+  res.send(req.user);
 });
 
 export default router;
