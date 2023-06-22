4ff4c7a7f149aa37d392ad05d8bdd3d640683e8f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-22 11:51:29 +0100

    removed unused stuff

--- src/routes.ts
+++ src/routes.ts
@@ -1,19 +1,14 @@
-import expres, { Request, Response } from "express";
+import expres from "express";
 import { signup } from "./controllers/signup";
 import { login } from "./controllers/login";
 import { verifyEmail } from "./controllers/verify";
 import { resendOTP } from "./controllers/resend";
 import { githubAuth } from "./controllers/github";
 import { githubAuthCallback } from "./controllers/github/callback";
-import { protect } from "./middlewares/authMiddleware";
-import { getUser } from "./controllers/user";
 import { logout } from "./controllers/logout";
 
 const router = expres.Router();
 
-router.route("/").get(protect, (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
 router.post("/signup", signup);
 router.post("/login", login);
 router.put("/verify", verifyEmail);
@@ -21,7 +16,5 @@ router.put("/verify/resend", resendOTP);
 router.get("/github", githubAuth);
 router.get("/github/callback/", githubAuthCallback);
 router.post("/logout", logout);
-
-router.get("/user", protect, getUser);
 
 export default router;
