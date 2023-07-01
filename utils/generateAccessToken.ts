d457e6852ff11fedd8f09d49283cf08bb277129e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-01 14:32:40 +0100

    ugh!!!!

--- utils/generateAccessToken.ts
+++ utils/generateAccessToken.ts
@@ -13,18 +13,21 @@ const generateAccessToken = (res: Response, uuid: string) => {
 
   const accessToken = jwt.sign(payload, secretKey, { expiresIn });
 
-  res.cookie("access_token", accessToken, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 60 * 60 * 1000,
-  });
-
-  res.cookie("exp", generate().dashed, {
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 60 * 60 * 1000,
-  });
+  res
+    .cookie("access_token", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 60 * 60 * 1000,
+    })
+    .send("ok");
+  res
+    .cookie("exp", generate().dashed, {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 60 * 60 * 1000,
+    })
+    .send("ok");
 };
 
 export { generateAccessToken };
