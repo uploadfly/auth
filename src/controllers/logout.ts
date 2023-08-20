4aa79362a48420881679907824cc6eb1abd86fdc
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-20 11:52:20 +0100

    logout

--- src/controllers/logout.ts
+++ src/controllers/logout.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 const logout = async (req: Request, res: Response) => {
   res.clearCookie("access_token");
-  res.clearCookie("exp");
+  res.clearCookie("refresh_token");
   res.sendStatus(200);
 };
 
