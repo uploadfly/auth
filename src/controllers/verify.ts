7d92d02880bdd313db5dbde67ba1934c18e25925
akinloluwami <akinkunmioye42@gmail.com>
2023-10-01 21:39:26 +0100

    ....

--- src/controllers/verify.ts
+++ src/controllers/verify.ts
@@ -29,16 +29,6 @@ const verifyEmail = async (req: Request, res: Response) => {
   }
 
   if (dayjs().isAfter(dayjs(user.otp_expiry))) {
-    await prisma.user.update({
-      where: {
-        uuid: user.uuid,
-      },
-      data: {
-        otp: "",
-        otp_expiry: null,
-      },
-    });
-
     return res.status(400).json({
       message: "Validation link has expired",
     });
