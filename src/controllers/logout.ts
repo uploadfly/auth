271b854804b06477226edbf102ddc1f9f9576a42
akinloluwami <akinkunmioye42@gmail.com>
2023-10-16 10:55:11 +0100

    feat: updated domain

--- src/controllers/logout.ts
+++ src/controllers/logout.ts
@@ -3,10 +3,10 @@ import { Request, Response } from "express";
 const logout = async (req: Request, res: Response) => {
   const isProd = process.env.NODE_ENV === "production";
   res.clearCookie("access_token", {
-    domain: isProd ? ".uploadfly.cloud" : undefined,
+    domain: isProd ? ".uploadfly.co" : undefined,
   });
   res.clearCookie("refresh_token", {
-    domain: isProd ? ".uploadfly.cloud" : undefined,
+    domain: isProd ? ".uploadfly.co" : undefined,
   });
   res.sendStatus(200);
 };
